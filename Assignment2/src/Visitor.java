7e2f51a377ed328e550a476d5a253fa8a7149897
pattriciaruth <p.das.11@student.scu.edu.au>
2024-11-21 13:17:41 +0800

    visitor constructor

--- Assignment2/src/Visitor.java
+++ Assignment2/src/Visitor.java
@@ -1,3 +1,16 @@
-public class Visitor {
+public class Visitor extends Person{
+    private String TicketType; 
+    //eg. regular, vip, kids, annual pass
+    private boolean HasAccessToAllRides;
 
+
+    //default constructor
+    public Visitor(){
+        super();
+    }
+
+    //Parameterized constructor
+    public Visitor(String Name, int Age, String Address, String TicketType, boolean HasAccessToAllRides){
+        
+    }
 }
