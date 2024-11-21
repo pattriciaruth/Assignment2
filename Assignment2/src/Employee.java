109f54d1b1fa83fb165532f1d8b228d70ca0fdfd
pattriciaruth <p.das.11@student.scu.edu.au>
2024-11-21 12:31:10 +0800

    employee with getter setter

--- Assignment2/src/Employee.java
+++ Assignment2/src/Employee.java
@@ -1,3 +1,35 @@
-public class Employee {
+
+public class Employee extends Person {
+    private String Jobtitle;
+    private double Salary;
+
+
+  //default constructor
+    public Employee(){
+        super();
+    }
+
+    public Employee(String Name, int Age, String Address, String Jobtitle, double Salary){
+        super(Name, Age, Address );
+        this.Jobtitle = Jobtitle;
+        this.Salary = Salary;
+    }
+
+    // Getters and Setters
+    public String getJobtitle() {
+        return Jobtitle;
+    }
+
+    public void setJobtitle(String Jobtitle ) {
+        this.Jobtitle = Jobtitle;
+    }
+
+    public double getSalary() {
+        return Salary;
+    }
+
+    public void setSalary(double Salary) {
+        this.Salary = Salary;
+    }
 
 }
