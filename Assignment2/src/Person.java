b26a6fb8056275c40115547e32e72e3de8156b0a
pattriciaruth <p.das.11@student.scu.edu.au>
2024-11-21 12:16:03 +0800

    class person with getter and setters

--- Assignment2/src/Person.java
+++ Assignment2/src/Person.java
@@ -1,3 +1,45 @@
 public class Person {
+    private String Name;
+    private int Age;
+    private String Address;
+
+
+    //default Constructor
+    public Person() {
+
+    }
+
+    //Parameterizing Constructor
+    public Person (String Name, int age, String Address){
+        this.Name = Name;
+        this.Age = Age;
+        this.Address = Address;
+        
+    }
+
+    //getters and setters
+    public String getName(){
+        return Name;
+    }
+
+    public void setName(String Name){
+        this.Name = Name;
+    }
+
+    public int getAge(){
+        return Age;
+    }
+
+    public void setAge(int Age){
+        this.Age = Age;
+    }
+
+    public String getAddress(){
+        return Address;
+    }
+
+    public void setAddress(String Address){
+        this.Address = Address;
+    }
 
 }
