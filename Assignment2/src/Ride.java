6ebb9a6cb18964c8482d6098359f92cd8891480e
pattriciaruth <p.das.11@student.scu.edu.au>
2024-11-29 10:50:55 +0800

    Part1 final commit

--- Assignment2/src/Ride.java
+++ Assignment2/src/Ride.java
@@ -1,3 +1,53 @@
 public class Ride {
+    private String name;
+    private String type; // e.g., Roller Coaster, Water Ride
+    private boolean isOpen;
+    private Employee operator;
 
+    // Default Constructor
+    public Ride() {
+        
+    }
+
+    // Parameterized Constructor
+    public Ride(String name, String type, boolean isOpen, Employee operator) {
+        this.name = name;
+        this.type = type;
+        this.isOpen = isOpen;
+        this.operator = operator;
+    }
+
+    // Getters and Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public boolean isOpen() {
+        return isOpen;
+    }
+
+    public void setOpen(boolean open) {
+        isOpen = open;
+    }
+
+    public Employee getOperator() {
+        return operator;
+    }
+
+    public void setOperator(Employee operator) {
+        this.operator = operator;
+    }
 }
+
--- Assignment2/src/Visitor.java
+++ Assignment2/src/Visitor.java
@@ -1,16 +1,35 @@
-public class Visitor extends Person{
-    private String TicketType; 
-    //eg. regular, vip, kids, annual pass
-    private boolean HasAccessToAllRides;
+public class Visitor extends Person {
+    private String ticketType; // e.g., Regular, VIP, Annual Pass
+    private boolean hasAccessToAllRides;
 
-
-    //default constructor
-    public Visitor(){
-        super();
+    // Default Constructor
+    public Visitor() {
+        super(); // Call the Person default constructor
+        this.ticketType = "Regular";
+        this.hasAccessToAllRides = false;
     }
 
-    //Parameterized constructor
-    public Visitor(String Name, int Age, String Address, String TicketType, boolean HasAccessToAllRides){
-        
+    // Parameterized Constructor
+    public Visitor(String name, int age, String address, String ticketType, boolean hasAccessToAllRides) {
+        super(name, age, address); // Call the Person parameterized constructor
+        this.ticketType = ticketType;
+        this.hasAccessToAllRides = hasAccessToAllRides;
+    }
+
+    // Getters and Setters
+    public String getTicketType() {
+        return ticketType;
+    }
+
+    public void setTicketType(String ticketType) {
+        this.ticketType = ticketType;
+    }
+
+    public boolean isHasAccessToAllRides() {
+        return hasAccessToAllRides;
+    }
+
+    public void setHasAccessToAllRides(boolean hasAccessToAllRides) {
+        this.hasAccessToAllRides = hasAccessToAllRides;
     }
 }
